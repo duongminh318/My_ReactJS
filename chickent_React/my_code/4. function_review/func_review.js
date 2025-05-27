6bf2eebd9fdd0c8e412e51c4676e49ae0c7bdfea
duongminh318 <duongminh318@gmail.com>
2025-05-27 14:28:48 +0700

    js function review, declaration and expression function

---
+++ chickent_React/my_code/4. function_review/func_review.js
@@ -0,0 +1,32 @@
+// khai báo hàm function declaration (tuyên bố)
+// function xinChao(){
+//     console.log("Hello, How are you");
+// }
+
+// xinChao();
+
+
+// function xinChaoBan(name){
+//     console.log(`Hello, How are you, ${name}`);
+// }
+
+// xinChaoBan("Vân");
+
+// tính tích 2 số
+// function tinhTich(soThuNhat, soThuHai){
+
+//     let tich= soThuNhat* soThuHai;
+//     console.log(`Đáp Án:  ${soThuNhat} x ${soThuHai} = ${tich}`);
+// }
+
+// tinhTich(3,4);
+
+// Function expression (sau dấu bằng và không có tên hàm)
+
+let tong = function (soA, soB) {
+    return soA + soB;
+}
+
+// gọi tên biến
+
+console.log(tong(2, 4));
