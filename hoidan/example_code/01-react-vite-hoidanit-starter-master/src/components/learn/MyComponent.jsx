b5a8fab937448fd752b52a29eb8d1a572723efbb
Win <duongminh318@gmail.com>
2025-07-06 21:14:49 +0700

    chapter 5, #31. cách sử dụng biến số với jsx

--- hoidan/example_code/01-react-vite-hoidanit-starter-master/src/components/learn/MyComponent.jsx
+++ hoidan/example_code/01-react-vite-hoidanit-starter-master/src/components/learn/MyComponent.jsx
@@ -15,15 +15,29 @@ import './style.css';
 
 
 const MyComponent = () => {
+    // const hoidanit= "eric 1" ; // string
+    // const hoidanit= 222;    // number
+    //  const hoidanit= true;    // boolean : ko in ra dc gì cả
+    //  const hoidanit= undefined;
+    //const hoidanit= null;
+    const hoidanit= [1, 2, 3 ];
+
+    // const hoidanit = {
+
+    //     name: "hoi dan IT",
+    //     age: 25
+    // }
+
+// JSON.stringify() = biến 1 object thành string
 
     return (
         <>
 
-            <div> Win & Win tech</div>
-            <div className="child" 
-            style={
-                {borderRadius: "10px"}
-            }
+            <div> {JSON.stringify(hoidanit)} Win & Win tech</div>
+            <div className="child"
+                style={
+                    { borderRadius: "10px" }
+                }
             > class name chill</div>
         </>
 
