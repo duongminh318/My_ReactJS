5ec1ed2d6bfe7311711e37767bd7a602a72f686a
Win <duongminh318@gmail.com>
2025-07-07 09:27:08 +0700

    chapter 5: #33, chia layout component

---
+++ hoidan/example_code/01-react-vite-hoidanit-starter-master/src/components/todo/TodoData.jsx
@@ -0,0 +1,12 @@
+const TodoData = () => {
+
+    return (
+        <div className="todo-data">
+            <div className="task-item">Học CSS</div>
+            <div className="task-item">Đi ngủ sớm</div>
+        </div>
+
+    )
+}
+
+export default TodoData;
---
+++ hoidan/example_code/01-react-vite-hoidanit-starter-master/src/components/todo/TodoNew.jsx
@@ -0,0 +1,11 @@
+const TodoNew = () => {
+    return (
+
+        <div className="todo-new">
+            <input type="text" placeholder="Thêm một công việc mới..." />
+            <button>Add</button>
+        </div>
+    );
+}
+
+export default TodoNew;
