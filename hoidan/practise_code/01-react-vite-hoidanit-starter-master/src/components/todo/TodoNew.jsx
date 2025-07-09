6add66c8190d6511d44974492d41779f56d72390
Win <duongminh318@gmail.com>
2025-07-09 08:58:54 +0700

    #36. DOM Events

--- hoidan/practise_code/01-react-vite-hoidanit-starter-master/src/components/todo/TodoNew.jsx
+++ hoidan/practise_code/01-react-vite-hoidanit-starter-master/src/components/todo/TodoNew.jsx
@@ -1,14 +1,33 @@
 const TodoNew = (props) => {
- console.log(">>> check props: ", props);
-    const {addNewToDo}= props;
+    console.log(">>> check props: ", props);
+    const { addNewToDo } = props;
 
-     addNewToDo("win");   // đưa đối số vào
-   
+    // addNewToDo("win");   // đưa đối số vào
+
+    // addNewTodo ("eric")  
+    const handleClick = () => {  // fire: kích hoạt function
+        alert("Click me")
+    }
+
+    //  const handleOnChange = (event) => {  // fire: kích hoạt function
+    //    console.log(">> handleOnChange ",event.target.value);
+    // }
+
+
+    const handleOnChange = (name) => {  // fire: kích hoạt function
+        console.log(">> handleOnChange ", name);
+    }
+
     return (
 
         <div className="todo-new">
-            <input type="text" placeholder="Thêm một công việc mới..." />
-            <button>Add</button>
+            <input type="text" placeholder="Thêm một công việc mới..."
+                onChange={(event) => handleOnChange(event.target.value)}  // thay bằng arrow function
+            />
+            <button style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add
+            </button>
         </div>
     );
 }
@@ -19,7 +38,7 @@ const TodoNew = (props) => {
 //     const {addNewToDo}= props;
 
 //     // addNewToDo("eric");
-   
+
 //     return (
 
 //         <div className="todo-new">
