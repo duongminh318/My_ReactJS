d506f3a598dfd80c1e525fec0aaae19e139503af
Win <duongminh318@gmail.com>
2025-07-28 19:49:39 +0700

    ga. 14.6 React js - React onClick  - tạo và gắn sự kiện cho một item html trong react

--- chickent_React/my_code/14.0 props-start/src/components/TabButton.jsx
+++ chickent_React/my_code/14.0 props-start/src/components/TabButton.jsx
@@ -1,7 +1,20 @@
 export default function TabButton({children}){
+
+  // sự kiện click trong js
+  // const btn = document.querySelector("button");
+  // btn.addEventListener("click", ()=>{
+  //   // làm việc gì đó
+
+  // })
+  
+// tạo sự kiện click trong react sau đó gắn sự kiện cho nút bấm 
+function handleClick() {
+  // làm việc gì đó khi nút được bấm
+  alert("nút bấm đã được click");
+}
     return(
          <li>
-              <button>{children} </button>
+              <button onClick={handleClick}>{children} </button>
             </li>
     )
 }
