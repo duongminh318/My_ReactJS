8c6e44c7162217fdb71d6f6b8c803a3b9a339c8d
Win <duongminh318@gmail.com>
2025-07-29 18:43:01 +0700

    ga. 14.9 State Tìm hiểu về State và hook trong reactjs

--- chickent_React/my_code/14.0 props-start/src/App.jsx
+++ chickent_React/my_code/14.0 props-start/src/App.jsx
@@ -2,6 +2,7 @@
 
 
 // import data 
+import { useState } from "react";
 import { myData } from "../data";
 // import Header 
 import Header from "./components/Header/Header";
@@ -16,18 +17,16 @@ import TabButton from "./components/TabButton.jsx"
 
 // ✅ App component – Thành phần chính chứa toàn bộ nội dung
 function App() {
+  const [selectedTopic, setselectedTopic] = useState("vui lòng click vào nút");
+  // console.log("App được gọi");
 
-  // tạo sự kiện click trong react sau đó gắn sự kiện cho nút bấm 
-  // function handleClick() {
-  //   // làm việc gì đó khi nút được bấm
-  //   alert("nút bấm đã được click");
-  // }
+
+  // console.log(`${tabContent} ngoài hàm`);
 
   // handerSelect 
-  function handleClick(selectedButton) {
-    alert(`Bạn đã chọn: ${selectedButton}`);
+  function handleSelect(selectedButton) {
+    setselectedTopic(selectedButton); // Đúng
   }
-
 
   return (
     <>
@@ -36,35 +35,36 @@ function App() {
 
       {/* Nội dung chính */}
       <main>
-      <section id="core-concepts">
-        <h2>Khái niệm chính trong React</h2>
+        <section id="core-concepts">
+          <h2>Khái niệm chính trong React</h2>
 
-        <ul>
-        <MainContent
-          image={myData[0].image}
-          title={myData[0].title}
-          desc={myData[0].desc}
-        />
-        {/* Hoặc có thể sử dụng cú pháp spread để truyền props ngắn gọn hơn */}
-        {/* <MainContent {...myData[0]} />  */}
-        <MainContent {...myData[1]} />
-        <MainContent {...myData[2]} />
-        <MainContent {...myData[3]} />
-        </ul>
-      </section>
+          <ul>
+            <MainContent
+              image={myData[0].image}
+              title={myData[0].title}
+              desc={myData[0].desc}
+            />
+            {/* Hoặc có thể sử dụng cú pháp spread để truyền props ngắn gọn hơn */}
+            {/* <MainContent {...myData[0]} />  */}
+            <MainContent {...myData[1]} />
+            <MainContent {...myData[2]} />
+            <MainContent {...myData[3]} />
+          </ul>
+        </section>
 
-      <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton onSelect={() => handleClick("Components")}>Components</TabButton>
-          <TabButton onSelect={() => handleClick("JSX")}>JSX</TabButton>
-          <TabButton onSelect={() => handleClick("Props")}>Props</TabButton>
-          <TabButton onSelect={() => handleClick("State")}>State</TabButton>
-        </menu>
-      </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>state</TabButton>
+          </menu>
+          {selectedTopic}
+        </section>
       </main>
     </>
-    );
+  );
 }
 
 
