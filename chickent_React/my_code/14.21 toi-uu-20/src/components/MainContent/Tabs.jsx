2ca66e943c0aed48f114f69dd0ca3398466ea45c
Win <duongminh318@gmail.com>
2025-08-17 09:41:51 +0700

    ga. 14.21 React Tips Tối ưu Tabs Menu cho ứng dụng lớn

---
+++ chickent_React/my_code/14.21 toi-uu-20/src/components/MainContent/Tabs.jsx
@@ -0,0 +1,11 @@
+export default function Tabs({children, button, ButtonsContainer}){
+
+    // const ButtonsContainer =buttonsContainer;
+    return(
+        <>
+            <ButtonsContainer>{button}</ButtonsContainer>
+            {children}
+        
+        </>
+    );
+}
