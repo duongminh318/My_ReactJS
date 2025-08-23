0ee0c63f3fcd379460c213d702ac4627ee5102bd
Win <duongminh318@gmail.com>
2025-08-23 09:38:10 +0700

    ga. 18.6 Sử dụng ref để xử lý Stop Timer - Refs & Portals trong React

--- chickent_React/my_code/18.2 React useRef/src/components/TimeStopper.jsx
+++ chickent_React/my_code/18.2 React useRef/src/components/TimeStopper.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimeStopper({ title, targetTime }) {
+  const timer= useRef();
   // Tạo 2 state để lưu thời gian
   const [timerStart, setTimerStart] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -9,7 +10,7 @@ export default function TimeStopper({ title, targetTime }) {
   // Đây là hàm sẽ được gọi khi bạn muốn bắt đầu bộ đếm.
   function handleStart() {
     // Dòng này đặt một "lệnh hẹn giờ" (timer) để chạy một đoạn code sau một khoảng thời gian nhất định.
-    setTimeout(() => {
+    timer.current= setTimeout(() => {
       // Đoạn code bên trong này chỉ chạy sau khi hết thời gian chờ.
       // Nó cập nhật state của "timer" thành "đã hết hạn".
       setTimerExpired(true);
@@ -20,6 +21,11 @@ export default function TimeStopper({ title, targetTime }) {
     setTimerStart(true);
   }
 
+  function handleStop(){
+    clearTimeout(timer.current);
+    setTimerStart(false);
+  }
+
   return (
     // Thẻ <section> bao bọc toàn bộ giao diện của component.
     <section className="challenge">
@@ -28,6 +34,7 @@ export default function TimeStopper({ title, targetTime }) {
 
       {/* Cách hiển thị có điều kiện: Nếu `timerExpired` là true, thì hiển thị thẻ <p> này. Nếu không thì không hiển thị. */}
       {timerExpired && <p> Bạn đã thua</p>}
+     
 
       {/* Đoạn code này để hiển thị thời gian. */}
       <p className="challenge-time">
@@ -38,14 +45,14 @@ export default function TimeStopper({ title, targetTime }) {
       </p>
 
       {/* Đây là nút bấm. */}
-      <button onClick={handleStart}>
+      <button onClick={timerStart ? handleStop : handleStart}>
         {/* Nội dung của nút sẽ thay đổi tùy thuộc vào trạng thái `timerStart`. */}
         {/* Nếu `timerStart` là true, nút hiển thị "stop", ngược lại là "start". */}
         {timerStart ? "stop" : "start"}
       </button>
 
       {/* Đoạn code này để hiển thị trạng thái của timer. */}
-      <p className={timerStart ? " active" : undefined}>
+      <p className={timerStart? "active": undefined}>
         {/* Nếu `timerStart` là true, thêm class "active" và hiển thị "Time is running". */}
         {/* Ngược lại, không có class và hiển thị "Timer inactive". */}
         {timerStart ? " Time is running" : "Timer inactive"}
