cee4d8223a6454c9954a8c4f70f94e1522e75ccc
Win <duongminh318@gmail.com>
2025-08-24 11:24:49 +0700

    ga. Giải Thích Portals Trong React_ 

--- chickent_React/my_code/18.2 React useRef/src/components/ResultModel.jsx
+++ chickent_React/my_code/18.2 React useRef/src/components/ResultModel.jsx
@@ -1,4 +1,7 @@
 import { useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
+
+
 export default function ResultModel({ targetTime, ref, remainingTime, onReset }) {
     const dialog = useRef();
     const userLost = remainingTime <= 0;
@@ -13,17 +16,19 @@ export default function ResultModel({ targetTime, ref, remainingTime, onReset })
             },
         };
     });
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal">
             {/* <h2>you {result}</h2> */}
             {userLost && <h2> You Lost</h2>}
+            {/* nếu người dùng chưa thua --> hiển thị điểm của bạn ra */}
             {!userLost && <h2> Your Score {score}</h2>}
             <p> thời gian đích : <strong> {targetTime} second</strong></p>
             <p> bạn còn <strong> {formattedRemainingTime} </strong> </p>
             <form method="dialog" onSubmit={onReset}>
                 <button> Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     )
 
 }
