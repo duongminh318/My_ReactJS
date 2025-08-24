b65df5facc558c587ea8c25a1dc55d5f4c4ff73a
Win <duongminh318@gmail.com>
2025-08-24 11:08:25 +0700

    ga. 18.11 Hiển thị kết quả – Tính điểm khi dừng đúng thời gian - Dự án thực hành refs, protals react js

--- chickent_React/my_code/18.2 React useRef/src/components/ResultModel.jsx
+++ chickent_React/my_code/18.2 React useRef/src/components/ResultModel.jsx
@@ -1,21 +1,23 @@
 import { useImperativeHandle, useRef } from "react"
-export default function ResultModel({ targetTime, ref, remainingTime, onReset}){
-
-    const userLost= remainingTime<=0;
-    const formattedRemainingTime= (remainingTime/1000).toFixed(2);
-
+export default function ResultModel({ targetTime, ref, remainingTime, onReset }) {
     const dialog = useRef();
-    useImperativeHandle(ref, ()=>{
-        return{
-            open(){
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    // biến điểm
+    const score = Math.round((1-remainingTime/(targetTime*1000))*100) // remainingTime: thời gian còn lại
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
                 dialog.current.showModal();
             },
         };
     });
-    return(
+    return (
         <dialog ref={dialog} className="result-modal">
             {/* <h2>you {result}</h2> */}
-            {userLost && <p> You Lost</p>}
+            {userLost && <h2> You Lost</h2>}
+            {!userLost && <h2> Your Score {score}</h2>}
             <p> thời gian đích : <strong> {targetTime} second</strong></p>
             <p> bạn còn <strong> {formattedRemainingTime} </strong> </p>
             <form method="dialog" onSubmit={onReset}>
