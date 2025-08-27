956663cc06b54bd8e9d208da2ccb04a324abdcf4
Win <duongminh318@gmail.com>
2025-08-27 14:54:34 +0700

    ga. 21.4 Instructions for useEffect with DOM_ Resize, Keydown events in ReactJS

---
+++ chickent_React/my_code/21 useeffect/21.4/src/App.jsx
@@ -0,0 +1,11 @@
+import Parent from "./componets/Parent.jsx";
+
+function App() {
+  return (
+    <>
+      <Parent />
+    </>
+  );
+}
+
+export default App;
---
+++ chickent_React/my_code/21 useeffect/21.4/src/componets/KeydownEvent.jsx
@@ -0,0 +1,30 @@
+import {useEffect} from "react";
+
+export default function KeydownEvent() {
+  useEffect(() => {
+    // Hàm xử lý resize
+    const handleKeyDown = (event) => {
+      console.log(event);
+      if (event.key) {
+        // Biểu tượng nhấn window + .
+        console.log("🌟 Phím được nhấn!");
+      }
+    };
+
+    // Đăng ký event
+    window.addEventListener("keydown", handleKeyDown);
+    console.log("Đã add event keydown!");
+
+    // === Bỏ cleanup để demo hậu quả khi mount/unmount liên tục ===
+    // return () => {
+    //   window.removeEventListener("keydown", handleKeyDown);
+    //   console.log("Đã remove event keydown!");
+    // };
+  }, []); // chỉ mount 1 lần
+
+  return (
+    <div>
+      <h3>Phím được nhấn !</h3>
+    </div>
+  );
+}
